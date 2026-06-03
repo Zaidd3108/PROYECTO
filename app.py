6227ed731aef84e1de5dd3fb2243fecaace90683
Zaidd3108 <machacuaykenneth@gmail.com>
2026-06-03 09:58:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "abarrotes_joel_2026"
 
 # LOGIN
 USUARIO_CORRECTO = "Zaidd"
