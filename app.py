3e9bcb68b3194e0cb50dd8386bc6d53e8fd7af3e
Zaidd3108 <machacuaykenneth@gmail.com>
2026-06-06 20:23:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.secret_key = "abarrotes_joel_2026"
-
-from werkzeug.utils import secure_filename
-import os
 
 UPLOAD_FOLDER = "static/img"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
