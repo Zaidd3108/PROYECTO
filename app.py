2d81194b41cf05c2942cb0030046737f55cfa4f7
Zaidd3108 <machacuaykenneth@gmail.com>
2026-06-17 10:18:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ import os
 app = Flask(__name__)
 app.secret_key = "abarrotes_joel_2026"
 
+from flask_mail import Mail, Message
+import random
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+
+app.config["MAIL_USERNAME"] = "supermercado.abarrotes.joel@gmail.com"
+app.config["MAIL_PASSWORD"] = "gfmh hibc hobp rpee"
+
+mail = Mail(app)
+
 UPLOAD_FOLDER = "static/img"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
@@ -757,3 +769,4 @@ def eliminar_compra(id):
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
